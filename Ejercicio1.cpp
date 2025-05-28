ddd0d591e654a352ec4931c5bc283ac68eb5cdbf
JackQuenta <jackalvaroqm@gmail.com>
2025-05-28 11:28:05 -0500

    Actualización del ejercicio1: Permite visualizar el enunciado y valida que los valores esten en el rango correcto

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,3 +1,6 @@
+/*1: Determinar si un alumno aprueba o reprueba un curso, sabiendo que aprobara si su
+promedio de tres calificaciones es mayor o igual a 70; reprueba en caso contrario.
+*/
 #include <iostream>
 using namespace std;
 int main (){
@@ -5,15 +8,24 @@ int main (){
 	int promedio, notaA, notaB, notaC;
 	cout<<"Ingrese la notaA"<<endl;
 	cin>>notaA;
-	cout<<"Ingrese la notaB"<<endl;
-	cin>>notaB;
-	cout<<"Ingrese la notaC"<<endl;
-	cin>>notaC;
-	promedio=(notaA+notaB+notaC)/3;
-	if(promedio>=70){
-		cout<<"Aprueba";
+	if (notaA >= 0 and notaA <= 200 ){
+		cout<<"Ingrese la notaB"<<endl;
+		cin>>notaB;
+			if (notaA >= 0 and notaA <= 200 ){
+				cout<<"Ingrese la notaC"<<endl;
+				cin>>notaC;
+					if (notaA >= 0 and notaA <= 200 ){
+						promedio=(notaA+notaB+notaC)/3;
+							if(promedio>=70){
+								cout<<"Aprueba";
+							}else{
+								cout<<"Reprueba";
+							}
+					}else{
+						cout<<"NotaC incorrecta"<<endl;}
+			}else{
+				cout<<"NotaB incorrecta"<<endl;}
 	}else{
-		cout<<"Reprueba";
-	}
+		cout<<"NotaA incorrecta"<<endl;}
 	return 0;
 }
