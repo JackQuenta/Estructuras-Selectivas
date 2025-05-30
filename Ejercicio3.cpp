536835bbc83ad708ece810451974a34be3511f5e
JackQuenta <jackalvaroqm@gmail.com>
2025-05-30 10:36:42 -0500

    Se actualizo el Ejercicio3.cpp de estructuras selectivas: Mejora en la interfaz, muestra enunciado, valida la cantidad sea mayor a 0

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -1,19 +1,24 @@
+/*3: Calcular el total a pagar por la compra de camisas. Si se compran tres camisas o más se
+aplica un descuento del 20% sobre el total de la compra y si son menos de tres camisas un
+descuento del 10%*/
 #include <iostream>
 using namespace std;
 int main (){
-	float montoc, cantcam, preciou, preciof;
+	double montocompra, cantcamisas, preciounitario, preciofinal;
 	cout<<"Para calcular el monto total a pagar se necesitan los siguientes datos: "<<endl;
 	cout<<"Ingrese la cantidad de camisas a comprar:"<<endl;
-	cin>>cantcam;
-	cout<<"Indique el precio unitario de cada camisa: "<<endl;
-	cin>>preciou;
-	montoc=cantcam*preciou;
-	if(cantcam>3){
-		preciof=montoc*0.8;	
+	cin>>cantcamisas;
+	if (cantcamisas<=0){
+		cout<<"Ingreso un valor incorrecto";
 	}
-	else{
-		preciof=montoc*0.9;
-	}
-	cout<<"El monto final a pagar es de: "<<preciof<<endl;
+	while (cantcamisas>0){
+		cout<<"Indique el precio unitario de cada camisa: "<<endl;
+		cin>>preciounitario;
+		montocompra=cantcamisas*preciounitario;
+		if(cantcamisas>3){
+			preciofinal=montocompra*0.8;	
+		}else{
+		preciofinal=montocompra*0.9;}
+	cout<<"El monto final a pagar es de: "<<preciofinal<<endl;}
 	return 0;
 }
